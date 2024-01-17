f42356c92df2a1391b67e2b6aa0c6e17d414cb1f
Xuan Dung Le <xuandung.le@hotmail.com>
2024-01-17 12:55:52 +0200

    Add option to allow specifying fintuned model

--- inference.py
+++ inference.py
@@ -14,7 +14,8 @@ def inference(input, model, tokenizer):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", help="model name", type=str, default="meta-llama/Llama-2-7b-chat-hf", nargs='?')
+    parser.add_argument("--model_name", help="original model name", type=str, default="meta-llama/Llama-2-7b-chat-hf", nargs='?')
+    parser.add_argument("--finetuned_model", help="finetuned model name", type=str, default="stable_model", nargs='?')
     parser.add_argument("--question", help="Provide your question to the model", type=str, default="Which location in Vietnam do you like the most?", nargs='?')
     args = parser.parse_args()
 
@@ -32,7 +33,7 @@ if __name__ == "__main__":
     
     # Load the model
     set_seed(42)
-    model_dir = "stable_model"
+    model_dir = args.finetuned_model
     model = AutoModelForCausalLM.from_pretrained(model_dir, local_files_only=True)
     tokenizer = AutoTokenizer.from_pretrained(model_dir, local_files_only=True)
 
